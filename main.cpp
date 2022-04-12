1a511fcfdfebfc4c2733848830601695a74b8279
jxgonz <johangonzalez7373@gmail.com>
2022-04-12 16:10:31 -0700

    Revert "main.cpp"
    This reverts commit a340bd95453f8328a09419d30d98d869f2b15779.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 
 #include "c-echo.h"
-using namespace std;
 
 int main(int argv, char** argc) {
-    cout << "Count of output: " << count(echo(argv,argc)) << endl;
+    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
 }
 
